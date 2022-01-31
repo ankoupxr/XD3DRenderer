4f315db3c2ea04c87572a4d1a9800188a124f3d3
unknown <13539878768@163.com>
2022-01-31 16:03:43 +0800

    第一次提交

---
+++ XD3DRendering/d3dUtil.cpp
@@ -0,0 +1,132 @@
+
+#include "d3dUtil.h"
+#include <comdef.h>
+#include <fstream>
+
+using Microsoft::WRL::ComPtr;
+
+DxException::DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber) :
+    ErrorCode(hr),
+    FunctionName(functionName),
+    Filename(filename),
+    LineNumber(lineNumber)
+{
+}
+
+bool d3dUtil::IsKeyDown(int vkeyCode)
+{
+    return (GetAsyncKeyState(vkeyCode) & 0x8000) != 0;
+}
+
+ComPtr<ID3DBlob> d3dUtil::LoadBinary(const std::wstring& filename)
+{
+    std::ifstream fin(filename, std::ios::binary);
+
+    fin.seekg(0, std::ios_base::end);
+    std::ifstream::pos_type size = (int)fin.tellg();
+    fin.seekg(0, std::ios_base::beg);
+
+    ComPtr<ID3DBlob> blob;
+    ThrowIfFailed(D3DCreateBlob(size, blob.GetAddressOf()));
+
+    fin.read((char*)blob->GetBufferPointer(), size);
+    fin.close();
+
+    return blob;
+}
+
+Microsoft::WRL::ComPtr<ID3D12Resource> d3dUtil::CreateDefaultBuffer(
+    ID3D12Device* device,
+    ID3D12GraphicsCommandList* cmdList,
+    const void* initData,
+    UINT64 byteSize,
+    Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer)
+{
+    ComPtr<ID3D12Resource> defaultBuffer;
+
+    // 创建实际的默认缓冲区资源。
+    ThrowIfFailed(device->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+        D3D12_HEAP_FLAG_NONE,
+        &CD3DX12_RESOURCE_DESC::Buffer(byteSize),
+		D3D12_RESOURCE_STATE_COMMON,
+        nullptr,
+        IID_PPV_ARGS(defaultBuffer.GetAddressOf())));
+
+    // In order to copy CPU memory data into our default buffer, we need to create
+    // 为了把数据从CPU复制到默认缓冲区，需要创建一个upload heap
+    // an intermediate upload heap. 
+    ThrowIfFailed(device->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+        &CD3DX12_RESOURCE_DESC::Buffer(byteSize),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr,
+        IID_PPV_ARGS(uploadBuffer.GetAddressOf())));
+
+
+    // Describe the data we want to copy into the default buffer.
+    // 描述想要复制到默认缓冲的数据
+    D3D12_SUBRESOURCE_DATA subResourceData = {};
+    subResourceData.pData = initData;/*指向系统内存数组的指针，
+                                     该数组包含用于初始化缓冲区的数据。
+                                     如果缓冲区可以存储n个顶点，那么系统数组必须至少包含n个顶点，
+                                     以便整个缓冲区可以已初始化。*/
+    subResourceData.RowPitch = byteSize;/*对于缓冲区，我们复制的数据大小（字节）。行间距*/
+    subResourceData.SlicePitch = subResourceData.RowPitch;/*对于缓冲区，我们复制的数据大小（字节）。深度*/
+
+    // Schedule to copy the data to the default buffer resource.  At a high level, the helper function UpdateSubresources
+    // will copy the CPU memory into the intermediate upload heap.  Then, using ID3D12CommandList::CopySubresourceRegion,
+    // the intermediate upload heap data will be copied to mBuffer.
+    // 计划将数据复制到默认缓冲区资源。在较高级别上，helper函数UpdateSubresources将把CPU内存复制到中间上载堆中。
+    // 然后，使用ID3D12CommandList：：copysubresourcegion，将中间上载堆数据复制到mBuffer。
+	cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(), 
+		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST));
+    UpdateSubresources<1>(cmdList, defaultBuffer.Get(), uploadBuffer.Get(), 0, 0, 1, &subResourceData);
+	cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(),
+		D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ));
+
+    // Note: uploadBuffer has to be kept alive after the above function calls because
+    // the command list has not been executed yet that performs the actual copy.
+    // The caller can Release the uploadBuffer after it knows the copy has been executed.
+
+
+    return defaultBuffer;
+}
+
+ComPtr<ID3DBlob> d3dUtil::CompileShader(
+	const std::wstring& filename,
+	const D3D_SHADER_MACRO* defines,
+	const std::string& entrypoint,
+	const std::string& target)
+{
+	UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)  
+	compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+	HRESULT hr = S_OK;
+
+	ComPtr<ID3DBlob> byteCode = nullptr;
+	ComPtr<ID3DBlob> errors;
+	hr = D3DCompileFromFile(filename.c_str(), defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		entrypoint.c_str(), target.c_str(), compileFlags, 0, &byteCode, &errors);
+
+	if(errors != nullptr)
+		OutputDebugStringA((char*)errors->GetBufferPointer());
+
+	ThrowIfFailed(hr);
+
+	return byteCode;
+}
+
+std::wstring DxException::ToString()const
+{
+    // Get the string description of the error code.
+    _com_error err(ErrorCode);
+    std::wstring msg = err.ErrorMessage();
+
+    return FunctionName + L" failed in " + Filename + L"; line " + std::to_wstring(LineNumber) + L"; error: " + msg;
+}
+
+
